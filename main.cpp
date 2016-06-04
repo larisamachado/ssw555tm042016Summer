5e1fc3107db86f437c964a2c612911862dd382e3
Clara Gail Ramos <Clara Gail Ramos>
2016-06-04 11:26:46 -0400

    added test file and refactored main

--- main.cpp
+++ main.cpp
@@ -1,30 +1,47 @@
-/*
- * CS 555 WS
- * Project 02 - GEDCOM parser
- * Clara Gail Ramos
- * I pledge my honor that I have abided by the Stevens Honor System.
+/* CS 555 WS
+ * Project 03 - GEDCOM parser : Save INDIs and FAMs
+ * Clara Ramos, Larisa Machado, Varun Kumar
+ * We pledge our honor that we have abided by the Stevens Honor System.
  */
 #include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
 
-/*
- * Global variables
+/* Global variables
  * data = current string pulled from GEDCOM file
  * tag = line's tag; if N/A, "Invalid tag"
+ * test = file that code is parsing through
+ * result = file where output is printed
  */
-string data, tag;
+string data, tag = "Invalid tag";
 fstream test, result;
 
-/*
- * Current line's level is 0
+
+/* Takes in the line's current level
+ * prints the level to output and output.txt
+ */
+void printLevel(string s) {
+	cout << s << '\n';
+	cout << tag << '\n';
+	result << s << endl;
+	result << tag << endl;
+}
+
+//prints what's currently in data and the rest of the line
+void restOfLine() {
+	cout << data;
+	result << data;
+	getline(test, data);
+	cout << data << '\n';
+	result << data << endl;
+}
+
+/* Current line's level is 0
  * parses through the line looking for applicable tag
  * prints the line, level, and tag to output and to output.txt
  */
 int foundAZero() {
-	cout << data << " ";
-	result << data << " ";
 	test >> data;
 	if(data == "HEAD" || data == "TRLR" || data == "NOTE") {
 		tag = data;
@@ -39,134 +56,81 @@ int foundAZero() {
 			cout << data << '\n';
 			result << data << endl;
 		} else {
-			tag = "Invalid tag";
-			cout << data;
-			result << data;
-			getline(test, data);
-			cout << data << '\n';
-			result << data << endl;
+			restOfLine();
 		}
 	} else {
-		tag = "Invalid tag";
-		cout << data;
-		result << data;
-		getline(test, data);
-		cout << data << '\n';
-		result << data << endl;
+		restOfLine();
 	}
-	cout << "0" << '\n';
-	cout << tag << '\n';
-	result << "0" << endl;
-	result << tag << endl;
 	return 0;
 }
 
-/*
- * Current line's level is 1
+/* Current line's level is 1
  * parses through the line looking for tag
  * prints line, level, and tag to output and output.txt
  */
 int foundAOne() {
-	cout << data << " ";
-	result << data << " ";
 	test >> data;
-	if(data == "NAME") {
+	if(data == "NAME" || data == "SEX" || data == "FAMC" || data == "FAMS" || data == "HUSB" || data == "WIFE" || data == "CHIL") {
 		tag = data;
-		cout << data;
-		result << data;
-
-		getline(test, data);
-		cout << data << '\n';
-		result << data << endl;
-	} else if(data == "SEX" || data == "FAMC" || data == "FAMS" || data == "HUSB" || data == "WIFE" || data == "CHIL") {
-		tag = data;
-		cout << data;
-		result << data;
-		getline(test, data);
-		cout << data << '\n';
-		result << data << endl;
+		restOfLine();
 	} else if(data == "BIRT" || data == "MARR" || data == "DIV") {
 		tag = data;
 		cout << data << '\n';
 		result << data << endl;
 	} else if(data == "DEAT") {
 		tag = data;
-		cout << data;
-		result << data;
-		getline(test, data);
-		cout << data << '\n';
-		result << data << endl;
+		restOfLine();
 	} else {
-		tag = "Invalid tag";
-		cout << data;
-		result << data;
-		getline(test, data);
-		cout << data << '\n';
-		result << data << endl;
+		restOfLine();
 	}
-	cout << "1" << '\n';
-	cout << tag << '\n';
-	result << "1" << endl;
-	result << tag << endl;
 	return 0;
 }
 
-/*
- * Current line's level is 2
+/* Current line's level is 2
  * parses through the line looking for tag
  * prints line, level, and tag to output and output.txt
  */
 int foundATwo() {
-	cout << data << " ";
-	result << data << " ";
 	test >> data;
 	if(data == "DATE") {
 		tag = data;
-		cout << data;
-		result << data;
-		getline(test, data);
-		cout << data << '\n';
-		result << data << endl;
+		restOfLine();
 	} else {
-		tag = "Invalid tag";
-		cout << data;
-		result << data;
-		getline(test, data);
-		cout << data << '\n';
-		result << data << endl;
+		restOfLine();
 	}
-	cout << "2" << '\n';
-	cout << tag << '\n';
-	result << "2" << endl;
-	result << tag << endl;
 	return 0;
 }
 
-/*
- * opens GEDCOM file and creates new output.txt
+/* opens GEDCOM file and creates new output.txt
  * reads the level of each line and runs appropriate function
  * if unrecognized, prints error message and terminates program
  */
 int main() {
+	string num;
 	test.open("GEDCOM_test.ged", ios::in);
 	result.open("output.txt", ios::out);
-
 	test >> data;
 	while(!test.eof()) {
+		tag = "Invalid tag";
+		cout << data << " ";
+		result << data << " ";
 		if(data[0] == '0') {
 			foundAZero();
+			num = "0";
 		} else if(data[0] == '1') {
 			foundAOne();
+			num = "1";
 		} else if(data[0] == '2') {
 			foundATwo();
+			num = "2";
 		} else {
 			cerr << "Error: unrecognizable level. Program Terminated" << '\n';
 			result << "Error: unrecognizable level. Program Terminated" << '\n';
 			return -1;
 		}
+		printLevel(num);
 		test >> data;
 	}
-
 	test.close();
 	result.close();
 	return 0;
